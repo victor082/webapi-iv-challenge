606611a8566b39b98e72aeddee0bb0c90a19e2bb
victorN <victornguyen082@gmail.com>
2019-05-30 16:10:07 -0400

    copy api

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const server = require('./api/server.js');
+
+// We must make the port dynamic
+const port = process.env.PORT || 4000; 
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
